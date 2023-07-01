f06c006574e5924203ae984ed672a6a74137332b
BeefCutlet <whysabi492@naver.com>
2023-07-01 12:14:45 +0900

    BubbleSort 알고리즘 최적화

--- src/algorithm/BubbleSort.java
+++ src/algorithm/BubbleSort.java
@@ -21,9 +21,41 @@ public class BubbleSort {
 		}
 	}
 	
+	static void sort2(int[] array) {
+		int length = array.length;
+		for (int i = 0; i < length - 1; i++) {
+			int count = 0;
+			for (int j = length - 1; j > i; j--) {
+				if (array[j - 1] > array[j]) {
+					swap(array, j - 1, j);
+					count++;
+				}
+			}
+			if (count == 0) break;
+		}
+	}
+	
+	static void sort3(int[] array) {
+		int length = array.length;
+		int k = 0;
+		while(k < length - 1) {
+			int last = length - 1;
+			for (int j = length - 1; j > k; j--) {
+				if (array[j - 1] > array[j]) {
+					swap(array, j - 1, j);
+					last = j;
+				}
+			}
+			k = last;
+		}
+	}
+	
 	public static void main(String[] args) {
 		int[] array = {5, 2, 6, 1, 7, 3};
-		sort(array);
+//		sort(array);
+//		sort2(array);
+		sort3(array);
 		System.out.println(Arrays.toString(array));
+		//결과 : 1, 2, 3, 5, 6, 7
 	}
 }
