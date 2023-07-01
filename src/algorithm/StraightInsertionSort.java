34f3e6502bba7974875b09ae02ffeee50b46f8b7
BeefCutlet <whysabi492@naver.com>
2023-07-01 18:06:10 +0900

    StraightInsertionSort 알고리즘

---
+++ src/algorithm/StraightInsertionSort.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class StraightInsertionSort {
+	
+	static void swap(int[] array, int index1, int index2) {
+		int temp = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp;
+	}
+	
+	static void sort(int[] array, int n) {
+		for (int i = 1; i < n; i++) {
+			int j;
+			int temp = array[i];
+			for (j = i; j > 0 && array[j - 1] > temp; j--) {
+				array[j] = array[j - 1];
+			}
+			array[j] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = {9, 1, 3, 4, 6, 7, 8};
+		sort(array, array.length);
+		System.out.println(Arrays.toString(array));
+		//결과 : 1, 3, 4, 6, 7, 8, 9
+	}
+}
