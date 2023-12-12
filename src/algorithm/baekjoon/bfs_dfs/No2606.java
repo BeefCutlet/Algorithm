1d54c68cc822bdc15d4a48d26771017c081e9ad8
OSU <whysabi492@naver.com>
2023-12-13 00:32:46 +0900

    패키지 구조 변경, 백준 문제 풀이 추가

---
+++ src/algorithm/baekjoon/bfs_dfs/No2606.java
@@ -0,0 +1,53 @@
+package algorithm.baekjoon.bfs_dfs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No2606 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(reader.readLine());
+        int M = Integer.parseInt(reader.readLine());
+        boolean[] visited = new boolean[N + 1];
+        int[][] matrix = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(reader.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            matrix[node1][node2] = 1;
+            matrix[node2][node1] = 1;
+        }
+
+        dfs(1, visited, matrix);
+
+        int count = 0;
+        for (boolean isVisited : visited) {
+            if (isVisited) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            count--;
+        }
+
+        writer.write(String.valueOf(count));
+        writer.flush();
+    }
+
+    static void dfs(int startNode, boolean[] visited, int[][] matrix) {
+        for (int i = 1; i < matrix[startNode].length; i++) {
+            if (matrix[startNode][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                dfs(i, visited, matrix);
+            }
+        }
+    }
+}
